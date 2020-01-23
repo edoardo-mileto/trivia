378c1ca504411f9c9f1c11d65c71dbb610db7750
edoardo <edomileto@gmail.com>
2020-01-23 19:03:31 +0100

    create login.php

---
+++ login.php
@@ -0,0 +1,29 @@
+<html>
+<head>
+    <title>Trivia Login</title>
+    <script>
+    	function testLogin(){
+
+    	}
+    </script>
+</head>
+
+<body>
+    <header>
+        <h1>aAAaaAAjacs!!!1!!11</h1>
+    </header>
+    <div>
+    	<form>
+    		<p><label>papere</label>
+    			<input type="text" name="username" id="username">
+    		</p><p>
+    			<label>password</label>
+    			<input type="password" name="password" id="password">
+    		</p><p>
+    			<button onclick="testLogin()">Login</button>
+    		</p>
+    	</form>
+    </div>
+</body>
+</html>
+    
