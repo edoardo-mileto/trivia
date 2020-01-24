f07031c2b6b5fb9df3d3a865466fc904a0a931d9
edoardo <edomileto@gmail.com>
2020-01-24 21:01:21 +0100

    poorbook's login update

--- login.php
+++ login.php
@@ -3,7 +3,36 @@
     <title>Trivia Login</title>
     <script>
     	function testLogin(){
+    		var http = new XMLHttpRequest();
+    		http.onreadystatechange = function() {
+    			if(this.readyState == 4 && this.status == 200){
+    				alert(this.responseText);
+    			}
+    		};
+    		var un = document.getElementById("username").value;
+    		var pw = document.getElementById("password").value;
 
+    		var param = "un=" + un + "&pw=" + pw;
+
+    		http.open('POST','127.0.0.1/poorbook/api/login.php', true);
+    		http.setRequestHeader("Content-Type","application/x-www-form-urlencoded");
+    		http.send(param);
+
+    		/*
+    		var data = new FormData();
+    		data.append("username","pippo");
+    		data.append("password","topolino");
+    		
+    		var data = JSON.stringify(
+    			{
+    				"username":"pippo",
+    				"password":"topolino"
+    			}
+    			);
+    		http.setRequestHeader("Content-type","application/json");
+    		http.send(data);
+    		alert('Konnichiwa');
+    		*/
     	}
     </script>
 </head>
