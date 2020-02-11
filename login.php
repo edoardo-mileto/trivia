95fca6ddd4f9214a300de2123214d7aa3058dcdb
Edoardo Mileto <edomileto@gmail.com>
2020-02-11 22:27:36 +0100

    Trivia update: working login through poorbook

--- login.php
+++ login.php
@@ -1,58 +1,66 @@
+<?php
+session_start();
+
+if (array_key_exists("username", $_SESSION)) {
+    header("location: ./index.php");
+    exit;
+}
+?>
+
+
+<!DOCTYPE html>
 <html>
 <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="login.css" rel="stylesheet" type="text/css">
     <title>Trivia Login</title>
     <script>
-    	function testLogin(){
-    		var http = new XMLHttpRequest();
-    		http.onreadystatechange = function() {
-    			if(this.readyState == 4 && this.status == 200){
-    				alert(this.responseText);
-    			}
-    		};
-    		var un = document.getElementById("username").value;
-    		var pw = document.getElementById("password").value;
+        function testLogin()
+        {
+            var http = new XMLHttpRequest();
+            http.onreadystatechange = function()
+            {
+                alert(this.readyState + " - " + this.status );
+                if(this.readyState == 4 && this.status == 200){
+                    alert(this.responseText);
+                    response = JSON.parse(this.responseText);
+                    alert(response.code);
+                    if(response.code==1){
+                        document.location.href="./index.html";
+                    }
+                    else{
+                        alert(response.description);
+                    }
+                }
+            };
 
-    		var param = "un=" + un + "&pw=" + pw;
+            
+            var un =document.getElementById("username").value;
+            var pw =document.getElementById("password").value;
+            var param = "email=" + un + "&password=" + pw;
 
-    		http.open('POST','127.0.0.1/poorbook/api/login.php', true);
-    		http.setRequestHeader("Content-Type","application/x-www-form-urlencoded");
-    		http.send(param);
+            http.open('POST','../poorbook/api/login.php',false);
+            http.setRequestHeader('Content-type','application/x-www-form-urlencoded');
+            http.send(param);
+        }
 
-    		/*
-    		var data = new FormData();
-    		data.append("username","pippo");
-    		data.append("password","topolino");
-    		
-    		var data = JSON.stringify(
-    			{
-    				"username":"pippo",
-    				"password":"topolino"
-    			}
-    			);
-    		http.setRequestHeader("Content-type","application/json");
-    		http.send(data);
-    		alert('Konnichiwa');
-    		*/
-    	}
     </script>
 </head>
-
 <body>
     <header>
-        <h1>aAAaaAAjacs!!!1!!11</h1>
+        <h1>TRIVIA!!!</h1>
     </header>
     <div>
-    	<form>
-    		<p><label>papere</label>
-    			<input type="text" name="username" id="username">
-    		</p><p>
-    			<label>password</label>
-    			<input type="password" name="password" id="password">
-    		</p><p>
-    			<button onclick="testLogin()">Login</button>
-    		</p>
-    	</form>
+        <!--form-->
+            <p>
+            <input type="email" id="username" placeholder="Username">
+            </p><p>
+            <input type="password" id="password" placeholder="Password">
+            </p><p>
+            <button onclick="testLogin()" type="submit">Login</button>
+            </p>
+        <!--/form-->
     </div>
 </body>
 </html>
-    
